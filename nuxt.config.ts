82b440552cc8ff829e9c771b0fd42d7ef8d9bbc9
iwasaki <poettype73@gmail.com>
2025-06-05 00:27:45 +0900

    Add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+  devtools: { enabled: false },
+  eslint: {
+    config: {
+      stylistic: {
+        blockSpacing: true,
+        indent: 2,
+        quotes: 'single',
+        semi: true,
+      },
+    },
+  },
+});
