9b6c07c6d1aa62099f4d1405835fdcd5a9ccb377
iwasaki <poettype73@gmail.com>
2025-06-06 00:27:24 +0900

    Add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxt/eslint'],
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/fonts'],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
   devtools: { enabled: false },
+  css: ['@/styles/styles.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/mixin.scss";',
+        },
+      },
+    },
+  },
   eslint: {
     config: {
       stylistic: {
