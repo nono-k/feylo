139623aa9be15f6c2c2b1f7bec0a11abf9f37c8f
iwasaki <poettype73@gmail.com>
2025-06-10 09:28:44 +0900

    Add data

---
+++ data/pages/home/heroData.ts
@@ -0,0 +1,12 @@
+export const heroData = [
+  {
+    title: ['Web制作に役立つ', 'ツールを紹介します'],
+    link: '/',
+    img: '/images/blog/image01.jpg',
+  },
+  {
+    title: ['aaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaa'],
+    link: '/',
+    img: 'https://placehold.jp/150x150.png',
+  },
+];
---
+++ data/pages/home/homeArchiveList.ts
@@ -0,0 +1,22 @@
+export const homeArchiveList = [
+  {
+    title: '初心者のためのJSネタ帳',
+    link: '/js-note',
+    desc: '初学者向けにJavaScriptの実践的な使い方をまとめてみました。',
+  },
+  {
+    title: 'Web制作に役立つツール集',
+    link: '/tools',
+    desc: '自作したWeb制作に役立つツールをまとめてみました。',
+  },
+  {
+    title: 'Webデザインアニメーション帳',
+    link: '/animation',
+    desc: 'css,JSを使ってリッチな表現になるアニメーションをまとめてみました。',
+  },
+  {
+    title: 'おすすめ本',
+    link: '/book',
+    desc: 'Web制作・プログラミングに限らずおすすめの本をまとめてみました。',
+  },
+];
