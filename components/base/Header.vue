5826751f5f64cf147ce92ecd4184c92ef3f68e01
iwasaki <poettype73@gmail.com>
2025-06-14 02:18:01 +0900

    Add Header

---
+++ components/base/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  backgroundColor?: string;
+  isComponentsMenu?: boolean;
+}>();
+</script>
+
+<template>
+  <header class="header container" :style="{ backgroundColor: backgroundColor }">
+    <ComponentsMenu v-if="isComponentsMenu" />
+    <h1 class="logo ff-zilla-slab-700-italic">
+      <NuxtLink to="/">Feylo</NuxtLink>
+    </h1>
+    <Hamburger />
+  </header>
+</template>
+
+<style scoped lang="scss">
+.logo {
+  font-size: 2.5rem;
+}
+</style>
