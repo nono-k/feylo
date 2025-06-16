5f8dcf734a74a203385bea9cb15257600368339b
iwasaki <poettype73@gmail.com>
2025-06-17 01:57:00 +0900

    Add Blog Page

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const { blog } = await useBlogContent(route.path);
+
+const breadcrumbItems = [
+  { path: '/', label: 'HOME' },
+  { path: '/blog', label: 'すべての記事一覧' },
+  { path: `/blog/${route.path}`, label: `${blog.value?.title}` },
+];
+
+useSeoMeta({
+  title: `${blog.value?.title} | Nuxt Content Demo`,
+  description: blog.value?.description,
+});
+</script>
+
+<template>
+  <main v-if="blog">
+    <Breadcrumb :items="breadcrumbItems" />
+    <BlogHeader :blog="blog" />
+
+    <div class="article">
+      <BlogMarkdownBody>
+        <p class="article__lead">{{ blog.description }}</p>
+
+        <BlogSummary
+          v-if="blog.summaryList"
+          :summary-text="blog.summaryText || 'この記事でわかること'"
+          :summary-list="blog.summaryList"
+        />
+
+        <Toc v-if="blog.body.toc?.links" :links="blog.body.toc.links" />
+
+        <ContentRenderer :value="blog" class="content" />
+
+        <SnsShare :title="blog.title" :slug="route.path" />
+      </BlogMarkdownBody>
+
+      <BlogSide />
+    </div>
+
+    <section class="section">
+      <HomeHeading text="ランダムな記事">Random</HomeHeading>
+      <RandomBlog />
+    </section>
+
+    <section class="section">
+      <Contact />
+    </section>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.article {
+  display: grid;
+  grid-template-columns: 73% 27%;
+  border-top: 1px solid var(--black);
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { blogs } = await useBlogContents();
+
+const breadcrumbItems = [
+  { path: '/', label: 'HOME' },
+  { path: '/blog', label: 'すべての記事一覧' },
+];
+</script>
+
+<template>
+  <main>
+    <Breadcrumb :items="breadcrumbItems" />
+    <section class="section">
+      <HomeHeading text="すべての記事一覧">Blog</HomeHeading>
+      <BlogTagList />
+      <BlogList v-if="blogs" :blogs="blogs" />
+    </section>
+    <section class="section">
+      <Contact />
+    </section>
+  </main>
+</template>
