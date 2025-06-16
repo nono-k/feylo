dfd6319cfb3021547980e0c689c1c5f95e04cd0f
iwasaki <poettype73@gmail.com>
2025-06-16 09:34:40 +0900

    Add HomeBlogList

---
+++ components/pages/home/HomeBlogList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { blogs } = await useBlogContents();
+</script>
+
+<template>
+  <div class="blog-list__wrap container">
+    <ul class="blog-list">
+      <li v-for="blog in blogs" :key="blog.id">
+        <CardTimeTop :data="blog" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.blog-list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  row-gap: 2rem;
+  margin-top: 3rem;
+  li {
+    border-right: 1px solid var(--black);
+    &:nth-child(4n) {
+      border-right: none;
+    }
+  }
+}
+</style>
