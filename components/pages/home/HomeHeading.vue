784eb981c4ca71fe4623bff66dd967daae651c1f
iwasaki <poettype73@gmail.com>
2025-06-16 09:35:43 +0900

    Add HomeHeading component

---
+++ components/pages/home/HomeHeading.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps<{
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+  text?: string;
+}>();
+
+const tag = `h${props.level || 2}`;
+</script>
+
+<template>
+  <div class="heading__wrap">
+    <div class="heading__deco ff-open-sans-700">
+      <slot />
+    </div>
+    <component :is="tag" class="heading container ff-open-sans-700">
+      <slot />
+    </component>
+    <p v-if="text" class="text container">{{ text }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.heading {
+  font-size: 8rem;
+  text-transform: uppercase;
+  line-height: 1.25;
+  &__deco {
+    font-size: 15rem;
+    height: 6.25rem;
+    line-height: 0.5;
+    border-top: 1px solid var(--black);
+    border-bottom: 1px solid var(--black);
+    text-transform: uppercase;
+    overflow: hidden;
+    color: var(--gray);
+  }
+}
+</style>
