8a38b0f1e501f8b9df63abca6498156d55b6f9dc
iwasaki <poettype73@gmail.com>
2025-06-20 00:28:12 +0900

    Add HomeNews

---
+++ components/pages/home/HomeNews.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="news__wrap">
+    <div class="news__heading ff-zilla-slab-700">news</div>
+    <a href="" class="news__link">ニュースです</a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.news {
+  &__wrap {
+    display: grid;
+    grid-template-columns: 8rem 1fr;
+    margin-top: 2rem;
+  }
+  &__heading {
+    background: var(--black);
+    color: var(--white);
+    font-size: 1.25rem;
+    text-transform: uppercase;
+    text-align: center;
+    padding-block: 0.2rem;
+  }
+  &__link {
+    padding-block: 0.2rem;
+    padding-inline: 0.5rem;
+    background: var(--purple);
+    font-weight: 500;
+  }
+}
+</style>
