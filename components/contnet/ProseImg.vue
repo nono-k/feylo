1ef0c464ca43ba1d1709fb48f9dda6a0e534d11d
iwasaki <poettype73@gmail.com>
2025-06-23 00:05:26 +0900

    Add ProseImg

---
+++ components/contnet/ProseImg.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: '',
+  },
+  height: {
+    type: [String, Number],
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <figure class="figure">
+    <div class="figure__img">
+      <img
+        :src="props.src"
+        :alt="props.alt"
+        :width="props.width"
+        :height="props.height"
+      />
+    </div>
+    <figcaption class="figure__caption">{{ props.alt }}</figcaption>
+  </figure>
+</template>
+
+<style scoped lang="scss">
+.figure {
+  max-width: 800px;
+  margin-inline: auto;
+  margin-block: 3rem 2rem;
+  &__caption {
+    margin-top: 0.5rem;
+    text-align: center;
+    font-size: 1rem;
+  }
+}
+</style>
