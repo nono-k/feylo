65b6c7aa2eaa8b4247e669668b9885dcd682a484
iwasaki <poettype73@gmail.com>
2025-07-02 00:10:12 +0900

    Fix Header

--- components/base/Hamburger.vue
+++ components/base/Hamburger.vue
@@ -2,7 +2,7 @@
 import { homeArchiveList } from '~/data/pages/home/homeArchiveList';
 
 const { blogs } = await useBlogContents();
-const newBlogs = blogs.value?.slice(0, 5);
+const newBlogs = computed(() => blogs.value?.slice(0, 5));
 
 const isOpen = ref(false);
 
@@ -13,6 +13,11 @@ const openMenu = () => {
 const closeMenu = () => {
   isOpen.value = false;
 };
+
+const router = useRouter();
+router.afterEach(() => {
+  isOpen.value = false;
+});
 </script>
 
 <template>
@@ -26,11 +31,23 @@ const closeMenu = () => {
   </button>
 
   <div :class="['hamburger-menu', isOpen ? 'is-open' : '']">
-    <div class="hamburger-menu__marquee">
-      <span class="hamburger-menu__text-marquee ff-zilla-slab-700">contact contact</span>
-      <span class="hamburger-menu__text-marquee ff-zilla-slab-700">contact contact</span>
-      <span class="hamburger-menu__text-marquee ff-zilla-slab-700">contact contact</span>
-    </div>
+    <a
+      href="https://forms.gle/NwTCisE5mEF55e6U7"
+      class="hamburger-menu__marquee for-large"
+      target="_blank"
+    >
+      <template v-for="n in 3" :key="n">
+        <NuxtMarquee
+          :auto-fill="true"
+          :speed="4"
+          :pause-on-hover="true"
+          :direction="n === 2 ? 'right' : 'left'"
+          :play="isOpen ? true : false"
+        >
+          <span class="hamburger-menu__text-marquee ff-zilla-slab-700">contact</span>
+        </NuxtMarquee>
+      </template>
+    </a>
     <div class="hamburger-menu__body">
       <div class="hamburger-menu__left">
         <div class="hamburger-menu__left-title ff-zilla-slab-700">Archive</div>
@@ -38,13 +55,18 @@ const closeMenu = () => {
           <li v-for="list, i in homeArchiveList" :key="list.title" class="hamburger-menu__left-item">
             <span class="hamburger-menu__left-num ff-zilla-slab-700-italic">{{ `0${i+1}` }}</span>
             <NuxtLink :to="list.link" class="hamburger-menu__left-link">
-              <span class="hamburger-menu__left-link-wrap">
+              <span
+                class="hamburger-menu__left-link-wrap"
+                :style="{
+                  '--bg-color': list.color,
+                }"
+              >
                 <span class="hamburger-menu__left-link-text">
                   <span class="hamburger-menu__left-link-title ff-zen-kaku-gothic-700">{{ list.title }}</span>
                   <span class="hamburger-menu__left-link-desc">{{ list.desc }}</span>
                 </span>
                 <span class="hamburger-menu__left-link-img">
-                  <img src="https://placehold.jp/150x150.png" alt="">
+                  <img :src="list.image" alt="" />
                 </span>
               </span>
             </NuxtLink>
@@ -52,17 +74,17 @@ const closeMenu = () => {
         </ul>
         <div class="hamburger-menu__left-sub-list-wrap">
           <ul class="hamburger-menu__left-sub-list">
-            <li class="hamburger-menu__left-sub-item">
+            <!-- <li class="hamburger-menu__left-sub-item">
               <NuxtLink to="/" class="hamburger-menu__left-sub-link">このサイトについて</NuxtLink>
+            </li> -->
+            <li class="hamburger-menu__left-sub-item">
+              <NuxtLink to="/profile" class="hamburger-menu__left-sub-link">運営者について</NuxtLink>
             </li>
             <li class="hamburger-menu__left-sub-item">
-              <NuxtLink to="/" class="hamburger-menu__left-sub-link">運営者について</NuxtLink>
+              <NuxtLink to="/site-policy" class="hamburger-menu__left-sub-link">サイトポリシー</NuxtLink>
             </li>
             <li class="hamburger-menu__left-sub-item">
-              <NuxtLink to="/" class="hamburger-menu__left-sub-link">ご利用規約</NuxtLink>
-            </li>
-            <li class="hamburger-menu__left-sub-item">
-              <NuxtLink to="/" class="hamburger-menu__left-sub-link">プライバシーポリシー</NuxtLink>
+              <NuxtLink to="/privacy-policy" class="hamburger-menu__left-sub-link">プライバシーポリシー</NuxtLink>
             </li>
           </ul>
           <ul class="hamburger-menu__left-sub-sns-wrap">
@@ -84,18 +106,18 @@ const closeMenu = () => {
           <div class="hamburger-menu__right-title-sub ff-zilla-slab-700">New</div>
           <div class="hamburger-menu__right-title ff-zilla-slab-700">最新記事</div>
         </div>
-        <ul class="hamburger-menu__right-list">
-          <li v-for="blog, i in newBlogs" :key="blog.id" class="hamburger-menu__right-item">
+        <ul v-if="newBlogs" class="hamburger-menu__right-list">
+          <li v-for="blog, i in newBlogs" :key="blog.path" class="hamburger-menu__right-item">
             <span class="hamburger-menu__right-num ff-zilla-slab-700-italic">{{ `0${i+1}` }}</span>
-            <NuxtLink :to="`${blog.path}`" class="hamburger-menu__right-link">
+            <NuxtLink :to="blog.path" class="hamburger-menu__right-link">
               <div class="hamburger-menu__right-link-text">
                 <div class="hamburger-menu__right-link-title">{{ blog.title }}</div>
-                <time :datetime="String(blog.date)" class="hamburger-menu__right-date">{{ parseDate(blog.date) }}</time>
+                <time class="hamburger-menu__right-date">{{ parseDate(blog.date) }}</time>
               </div>
               <div class="hamburger-menu__right-img">
                 <img :src="blog.image" :alt="blog.title" />
               </div>
-            </Nuxtlink>
+            </NuxtLink>
           </li>
         </ul>
         <a class="hamburger-menu__right-more ff-open-sans-700" href="/blog">View More</a>
@@ -122,6 +144,15 @@ const closeMenu = () => {
   outline: 0.125rem solid var(--white);
   z-index: 99;
   transition: 0.3s ease-in-out;
+  @include mixin.mobile {
+    right: 1rem;
+  }
+  @include mixin.phone {
+    padding: 0.5rem;
+    font-size: 0.75rem;
+    gap: 0.5rem;
+    top: 0.5rem;
+  }
   @include mixin.hover {
     translate: 2px 2px;
     box-shadow: 0 0 0 var(--black);
@@ -152,6 +183,9 @@ const closeMenu = () => {
   opacity: 0;
   transition: opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
   transition-delay: 0.8s;
+  @include mixin.mobile {
+    grid-template-columns: 1fr;
+  }
   &.is-open {
     visibility: visible;
     opacity: 1;
@@ -171,12 +205,15 @@ const closeMenu = () => {
     background-color: var(--black);
     clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0 100%);
     transition: clip-path 0.8s cubic-bezier(0.87, 0, 0.13, 1);
+    & > * + * {
+      margin-top: 1rem;
+    }
   }
   &__text-marquee {
     writing-mode: sideways-lr;
     color: var(--white);
     line-height: 1;
-    font-size: 2rem;
+    font-size: 2.8rem;
     text-transform: uppercase;
   }
   &__body {
@@ -186,12 +223,22 @@ const closeMenu = () => {
     clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0 100%);
     transition: clip-path 0.8s cubic-bezier(0.87, 0, 0.13, 1);
     transition-delay: 0.1s;
+    overflow-y: auto;
+    @include mixin.mobile {
+      grid-template-columns: 1fr 1fr;
+    }
+    @include mixin.phone {
+      grid-template-columns: 1fr;
+    }
   }
   &__left {
     background-color: var(--gray);
     display: flex;
     flex-direction: column;
     padding-bottom: 1rem;
+    @include mixin.phone {
+      order: 2;
+    }
   }
   &__close {
     position: absolute;
@@ -207,6 +254,9 @@ const closeMenu = () => {
     opacity: 0;
     transition: opacity 0.3s;
     transition-delay: 0s;
+    @include mixin.mobile {
+      right: 1rem;
+    }
   }
   &__left-title {
     padding-block: 1rem;
@@ -218,26 +268,6 @@ const closeMenu = () => {
     display: grid;
     grid-template-columns: 2rem 1fr;
     border-bottom: 1px solid var(--black);
-    &:nth-child(1) {
-      .hamburger-menu__left-link-wrap {
-        background: var(--yellow);
-      }
-    }
-    &:nth-child(2) {
-      .hamburger-menu__left-link-wrap {
-        background: var(--sky-blue);
-      }
-    }
-    &:nth-child(3) {
-      .hamburger-menu__left-link-wrap {
-        background: var(--orange);
-      }
-    }
-    &:nth-child(4) {
-      .hamburger-menu__left-link-wrap {
-        background: var(--purple);
-      }
-    }
   }
   &__left-num {
     display: flex;
@@ -258,6 +288,12 @@ const closeMenu = () => {
     grid-template-columns: 1fr 5rem;
     gap: 1.25rem;
     padding: 0.5rem 1.25rem 0.5rem;
+    background-color: var(--bg-color);
+    @include mixin.phone {
+      grid-template-columns: 1fr 3.5rem;
+      align-items: center;
+      padding: 1rem;
+    }
   }
   &__left-link-title {
     display: block;
@@ -276,12 +312,18 @@ const closeMenu = () => {
     transition-property: rotate;
     transition-duration: 0.57s;
     transition-timing-function: cubic-bezier(0.28, 2.5, 0.58, 0.73);
+    @include mixin.phone {
+      height: fit-content;
+    }
   }
   &__left-sub-list-wrap {
     display: grid;
     grid-template-columns: 1fr 1fr;
     margin-top: auto;
     margin-inline: 0.875rem;
+    @include mixin.phone {
+      margin-top: 2rem;
+    }
   }
   &__left-sub-sns-wrap {
     display: flex;
@@ -304,6 +346,9 @@ const closeMenu = () => {
     flex-direction: column;
     justify-content: flex-end;
     padding-bottom: 1rem;
+    @include mixin.phone {
+      order: 1;
+    }
   }
   &__right-title-wrap {
     color: var(--white);
@@ -311,6 +356,10 @@ const closeMenu = () => {
     border-top: 1px solid var(--white);
     border-bottom: 1px solid var(--white);
     padding-block: 0.75rem;
+    @include mixin.phone {
+      border-top: none;
+      padding-top: 1.25rem;
+    }
   }
   &__right-title {
     font-size: 1.25rem;
--- components/base/Header.vue
+++ components/base/Header.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 defineProps<{
   backgroundColor?: string;
-  isComponentsMenu?: boolean;
 }>();
 </script>
 
 <template>
   <header class="header container" :style="{ backgroundColor: backgroundColor }">
-    <ComponentsMenu v-if="isComponentsMenu" />
     <h1 class="logo ff-zilla-slab-700-italic">
       <NuxtLink to="/">Feylo</NuxtLink>
     </h1>
@@ -18,5 +16,8 @@ defineProps<{
 <style scoped lang="scss">
 .logo {
   font-size: 2.5rem;
+  @include mixin.phone {
+    font-size: 2rem;
+  }
 }
 </style>
