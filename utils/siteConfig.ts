1a2880af0138c464debf41e44a138d011a9d50e8
iwasaki <poettype73@gmail.com>
2025-07-04 01:23:53 +0900

    Add SiteConfig

---
+++ utils/siteConfig.ts
@@ -0,0 +1,15 @@
+type SiteConfig = {
+  siteTitle: string;
+  siteDesc: string;
+  siteUrl: string;
+  siteType: string;
+  siteImg: string;
+};
+
+export const siteConfig: SiteConfig = {
+  siteTitle: 'Feylo',
+  siteDesc: 'Web制作についてのメディアサイト「Feylo」です。初学者に向けて、Web制作の基礎から応用までを分かりやすく解説します。',
+  siteUrl: 'https://feylo.dev',
+  siteType: 'website',
+  siteImg: '/OGP.png',
+};
