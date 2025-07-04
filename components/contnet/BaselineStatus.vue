292ec20f3e976591bb47cb61b0c51dcca9d5b7be
iwasaki <poettype73@gmail.com>
2025-07-05 00:59:59 +0900

    Add baseline-status

---
+++ components/contnet/BaselineStatus.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+if (import.meta.client) {
+  const script = document.createElement('script');
+  script.type = 'module';
+  script.src = 'https://cdn.jsdelivr.net/npm/baseline-status@1/baseline-status.min.js';
+  document.head.appendChild(script);
+}
+
+const props = defineProps<{
+  featureId: string;
+}>();
+</script>
+
+<template>
+  <baseline-status :feature-id="props.featureId"></baseline-status>
+</template>
+
+<style scoped lang="scss">
+baseline-status {
+  margin-block: 2rem;
+  padding-top: 1rem;
+  padding-inline: 1.5rem;
+}
+</style>
