9bae522213383e7fb1e6591d41421a75fb9ef24f
iwasaki <poettype73@gmail.com>
2025-07-09 00:59:27 +0900

    Fix Book Data

--- data/pages/book/bookJavascript.ts
+++ data/pages/book/bookJavascript.ts
@@ -3,19 +3,19 @@ import type { BookType } from './bookType';
 export const bookJavascript: BookType[] = [
   {
     name: '1冊ですべて身につくJavaScript入門講座',
-    link: '3EzEZsN',
+    link: '44QJKZJ',
     image: '81KbmzH9wrL._SY522_',
     desc: '以外にWeb製作でJavaScriptでアニメーションについて書かれた書籍は少ないので、Web製作でのJavaScriptの基本を学びたい方はおすすめです！',
   },
   {
     name: '独習JavaScript 新版',
-    link: '3WJ96Ve',
+    link: '45TdMx3',
     image: '812SwsJIcQL._SY522_',
     desc: 'JavaScriptの独習シリーズです。JavaScriptの詳しい言語仕様を書籍で体系的に学びたい人におすすめの1冊になります。教科書的に手元に置いておくと良いでしょう。',
   },
   {
     name: '作って学ぶ　Next.js/React　Webサイト構築',
-    link: '42K3vBS',
+    link: '3GAvcEl',
     image: '71+GR1uVBDL._SY522_',
     desc: '本書はJavaScriptのフレームワークであるNext.jsとmicroCMSのJamstackの構成でブログを作る方法を実践できます。Reactなどのフレームワークを使ったことがない人でも分かりやすく書かれているので、この書籍でモダンな開発環境について学んでみてはいかがでしょうか。',
   },
